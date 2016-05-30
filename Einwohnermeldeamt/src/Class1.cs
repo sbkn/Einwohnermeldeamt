b3aacca538d677152df7a0df58320a3de1271a0c
appelblim <sergeybakin@googlemail.com>
2016-05-30 05:33:10 +0200

    init

---
+++ Einwohnermeldeamt/src/Class1.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace GameProject
+{
+    public class Player
+    {
+        public int positionX { get; set; }
+        public int positionY { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public Texture2D image { get; set; }
+        public int centerX { get; set; }
+        public int centerY { get; set; }
+
+        public Player(int x, int y, int h, int w)
+        {
+            positionX = x;
+            positionY = y;
+
+            height = h;
+            width = w;
+
+            centerX = positionX + width / 2;
+
+            centerY = positionY + height / 2;
+        }
+
+        public void move(int x, int y)
+        {
+            positionX += x;
+            positionY += y;
+        }
+    }
+}
